57496924eaf48a76e1efadc6510840aef87ffe8f
lavender <lavenderanyango9@gmail.com>
2023-02-16 05:46:32 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
+
