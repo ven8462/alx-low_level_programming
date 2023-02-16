32b19fac1480c13e5a977f9ef1bbd65530c9cb50
lavender <lavenderanyango9@gmail.com>
2023-02-16 04:54:43 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle\n");
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
 
