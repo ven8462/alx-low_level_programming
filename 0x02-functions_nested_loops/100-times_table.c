490c9fec8aeb9ceb6866fd4585aa7fdbfaee79f3
lavender <lavenderanyango9@gmail.com>
2023-02-21 13:07:11 -0800

    readme

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,21 @@
  */
 void print_times_table(int n)
 {
+if
+(n < 0 || n > 15);
+return;
 
-	return (0);
+int i, j, product;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++
+{
+for product = i * j;
+if (j == 0)
+_putchar("%d", product);
+else
+_putchar("%4d", product);
+}
+_putchar("\n");
+}
 }
