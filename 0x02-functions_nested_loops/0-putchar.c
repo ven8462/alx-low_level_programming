61d2b919e0f883ab2c0f84a605eeefc5fa044868
lavender <lavenderanyango9@gmail.com>
2023-02-21 04:08:19 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
 * main - Entry point
-* Decription: prints _putchar followed by a new line.
+* Description: prints _putchar followed by a new line
 *
-* Return: zero (0)
+* Return: zero(0)
 */
 int main(void)
 {
--putchar("_");
--putchar("p");
--putchar("u");
--putchar("t");
--putchar("c");
--putchar("h");
--putchar("a");
--putchar("r");
--putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-return (0);
+return(0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_alphabet(void);
 int _putchar(char c);
 #endif
