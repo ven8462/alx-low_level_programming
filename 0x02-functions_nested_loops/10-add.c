766763e32602b2ad9b30d1eab937b38da37d42f6
lavender <lavenderanyango9@gmail.com>
2023-02-21 07:16:07 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @a: first number input
+ * @b: second number input
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,6 @@ int _abs(int);
 int _islower(int c);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 int _putchar(char c);
 #endif
