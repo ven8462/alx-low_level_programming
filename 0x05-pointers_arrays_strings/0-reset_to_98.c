7f76b6628c38539b056e35ae5128eee91a96259b
lavender <lavenderanyango9@gmail.com>
2023-02-27 07:19:22 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Takes a pointer to a parameter and updates its value
+* @n: input integer.
+*
+* Return: no return
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
