2cf0cf105a41446c0f2ac9718d2159ce310ab777
lavender <lavenderanyango9@gmail.com>
2023-03-10 06:19:52 -0800

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
-int _putchar(char c);
+
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-        int i = 0;
+	int i;
 
-        while (*(str + i) != '\0')
-        {
-                _putchar(*(str + i));
-                i++;
-        }
-        _putchar('\n');
-} 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
