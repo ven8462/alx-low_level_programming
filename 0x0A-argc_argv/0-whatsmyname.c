9dd1232b2f84b4a0e2122ef409431e685a78eaf3
lavender <lavenderanyango9@gmail.com>
2023-03-10 10:55:38 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of a program
+* @argc: array size
+* @argv: array containing elements
+*
+* Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+	printf("%s\n",argv[argc - 1]);
+	return (0);
+}
