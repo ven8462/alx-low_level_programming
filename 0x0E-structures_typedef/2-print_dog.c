0ca8b12f86ea683258b6450e7734b7f2f474dc9f
lavender <lavenderanyango9@gmail.com>
2023-03-20 12:36:03 -0700

    task 1

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - initialize a variable of type `struct dog`
+ * @d: struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+		(*d).name != NULL ? d->name : "(nil)",
+		(*d).age,
+		(*d).owner != NULL ? d->owner : "(nil)");
+	}
+}
