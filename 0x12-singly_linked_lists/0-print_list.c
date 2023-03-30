0dabc8eed2a237f2f44f623b5a611c878f48ba26
Lavender <lavenderanyango9@gmail.com>
2023-03-30 10:53:44 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* print_list - prints all the elements of a list
+*
+* @h:pointer to the list
+*
+*Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
