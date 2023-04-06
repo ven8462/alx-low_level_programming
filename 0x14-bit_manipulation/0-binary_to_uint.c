82a0137174982ac603726b952e0fbbe904c27e09
Lavender <lavenderanyango9@gmail.com>
2023-04-06 06:45:15 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-	if (*b != '0' && *b != '1')
+	if (b[i] < '0' || b[i] > '1')
 		return (0);
 
 	value = value * 2 + (b[i] - '0');
