c602cde7ba91fe5b3d5836d9ed65cf637279b934
Lavender <lavenderanyango9@gmail.com>
2023-04-06 09:52:33 -0700

    binary representation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <unistd.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 
 #endif
