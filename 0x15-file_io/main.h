497cb04ea7c2e58e963e660338f09eddcc70c215
lavender <lavenderanyango9@gmail.com>
2023-04-29 02:08:52 -0700

    advanced task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,13 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void verify(unsigned char *e_ident);
+void magic(unsigned char *e_ident);
+void class(unsigned char *e_ident);
+void data(unsigned char *e_ident);
+void version(unsigned char *e_ident);
+void osabi(unsigned char *e_ident);
+void type(unsigned int e_type, unsigned char *e_ident);
+void entry(unsigned int e_entry, unsigned char *e_ident);
 #endif
