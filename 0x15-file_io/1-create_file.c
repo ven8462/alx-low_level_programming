5a140e09d90f284dff3cbd89908caaf5fa5de95e
Lavender <lavenderanyango9@gmail.com>
2023-05-08 15:59:06 -0700

    debugging task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,8 +27,10 @@ int create_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
+	{
 		return (-1);
+	}
 
-		close(o);
-		return (1);
+	close(o);
+	return (1);
 }
