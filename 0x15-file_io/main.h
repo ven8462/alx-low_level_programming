eca07ea66254c09519af2d7aba44ae20dc394b67
Lavender <lavenderanyango9@gmail.com>
2023-05-08 18:44:11 -0700

    display info in ELF

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,10 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-
-
-
-
-
+void print_data(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned int e_entry, unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void verify(unsigned char *e_ident);
 
 #endif
