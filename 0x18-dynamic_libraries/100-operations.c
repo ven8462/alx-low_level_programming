7413b8c63ee153110c921d3870d38fd9462f9c15
Lavender <lavenderanyango9@gmail.com>
2023-06-20 14:16:16 -0700

    creating a dynamic lib from c that can be used in py

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,58 @@
+#include "ven.h"
+
+/**
+ * add - add 2 integers
+ * @a: inte
+ * @b: int
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub 2 nums
+ * @a: int
+ * @b: int
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply 2 nums
+ * @a: int
+ * @b: int
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division 2 nums
+ * @a: int
+ * @b: int
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod 2 nums
+ * @a: int
+ * @b: int
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/ven.h
@@ -0,0 +1,10 @@
+#ifndef VEN_H
+#define VEN_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
